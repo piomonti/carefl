e7a70fb7aceb5dd4a6f6605ae19b098c498910c6
Ilyes <ilyes.khemakhem@gmail.com>
2020-07-04 16:12:47 +0100

    fix bug in CEP and optimize use/imports

--- runners/cause_effect_pairs_runner.py
+++ runners/cause_effect_pairs_runner.py
@@ -2,88 +2,61 @@
 #
 #
 
-import itertools
-
-import numpy as np
-import matplotlib.pyplot as plt
+from CauseEffectPairs.Run_CEP_01 import runCEPair
 import os
 
-import torch
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.nn.init as init
-from torch import nn
-from torch import distributions
-from torch.distributions import MultivariateNormal, Uniform, TransformedDistribution, SigmoidTransform
-from torch.nn.parameter import Parameter
 
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-from torchvision import datasets, transforms
-from torch.autograd import Variable
-import torch.autograd as autograd
-from torch.utils.data import DataLoader
+def RunCauseEffectPairs():
+    # define some simulation parameters
+    skipPairs = [52, 54, 55]
+    # skip these pairs, as indicated by Mooij et al (2016) because the variables
+    # are not bivariate (i.e., X and Y are not univariate)
 
-# load flows
-from nflib.flows import AffineConstantFlow, MAF, NormalizingFlowModel, Invertible1x1Conv, ActNorm
-from nflib.spline_flows import NSF_AR, NSF_CL
-from CauseEffectPairs.Run_CEP_01 import * 
-from models.classConditionalFlow import Flow 
-from models.bivariateFlowCD import BivariateFlowCD
-from sklearn.decomposition import FastICA, PCA
-import pickle 
+    saveDir = 'results/'
+    SplitPerc = .8
+    scaleDat = True
+    priorDist = 'laplace'
+    epochs = 500 + 250
+    optMethod = 'schedule'
 
-# define some simulation parameters
-skipPairs = [ 52, 54, 55 ] # skip these pairs, as indicated by Mooij et al (2016) because the variables are not bivariate (i.e., X and Y are not univariate)
+    correctCount = 0
+    runningCount = 0
 
-saveDir  = '/CauseEffectPairs/results/'
-SplitPerc = .8 
-scaleDat  = True
-priorDist = 'laplace' 
-epochs    = 500 + 250
-optMethod = 'schedule'
+    BinaryCutoff = .15
+    correctCount_nobinary = 0
+    runningCount_nobinary = 0
 
-correctCount = 0 
-runningCount = 0
+    LayerList = [1, 3]
+    depthList = [5]
 
-BinaryCutoff = .15
-correctCount_nobinary = 0
-runningCount_nobinary = 0
+    for i in range(1, 108):
+        if i in skipPairs:
+            pass
+        else:
+            res, predModel, trueModel, ctsRatio = runCEPair(i, LayerList, depthList, priorDist=priorDist,
+                                                            TrainSplit=SplitPerc, epochs=epochs, optMethod=optMethod,
+                                                            scaleDat=scaleDat, verbose=True)
 
-LayerList = [1,3] 
-depthList = [5] 
+            runningCount += 1
+            if ctsRatio > BinaryCutoff:
+                runningCount_nobinary += 1
+            if predModel.replace('-', '') == trueModel.replace('-', ''):
+                print('Correct!')
+                correctCount += 1
+                if ctsRatio > BinaryCutoff:
+                    correctCount_nobinary += 1
 
-PairDataDir = 'CauseEffectPairs/pairs/'
+            # save results
+            fileEnd = '.csv'
+            if scaleDat:
+                fileEnd = '_Scaled' + fileEnd
+            else:
+                fileEnd = '_Unscaled' + fileEnd
 
-for i in range(1, 108):
-	if i in skipPairs:
-		pass
-	else:
-		res, predModel, trueModel, ctsRatio = runCEPair( i, LayerList, depthList, priorDist=priorDist, TrainSplit=SplitPerc, epochs=epochs, optMethod=optMethod, scaleDat=scaleDat, verbose=True )
+            # add prior dist
+            fileEnd = '_' + priorDist + fileEnd
 
-		runningCount += 1
-		if ctsRatio > BinaryCutoff:
-			runningCount_nobinary += 1
-		if predModel.replace('-', '') == trueModel.replace('-', ''):
-			print('Correct!')
-			correctCount += 1 
-			if ctsRatio > BinaryCutoff:
-				correctCount_nobinary += 1
-
-		# save results
-		fileEnd = '.csv'
-		if scaleDat:
-			fileEnd = '_Scaled' + fileEnd
-		else:
-			fileEnd = '_Unscaled' + fileEnd
-
-		# add prior dist
-		fileEnd = '_' + priorDist + fileEnd
-	
-		print( os.getcwd() )
-		#res.to_csv( saveDir + 'pair' +str(i) + '_split' + str(int(SplitPerc*100)) + '_Flow' + fileEnd)
-		# print progress
-		print('running mean: ' + str(float(correctCount) / runningCount) )
-
+            print(os.getcwd())
+            # res.to_csv( saveDir + 'pair' +str(i) + '_split' + str(int(SplitPerc*100)) + '_Flow' + fileEnd)
+            # print progress
+            print('running mean: ' + str(float(correctCount) / runningCount))
